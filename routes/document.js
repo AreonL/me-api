585b374d1455fc2aa44f43185d0398d5c67a9d42
AreonL <areon.lundkvist@gmail.com>
2021-09-29 13:11:33 +0200

    updating await

--- routes/document.js
+++ routes/document.js
@@ -3,15 +3,15 @@ var router = express.Router();
 const data = require('../models/data');
 
 router.get('/', async function(req, res) {
-    data.getAllData(req, res);
+    await data.getAllData(req, res);
 });
 
 router.post('/create', (req, res) => {
-    data.createNewData(req, res);
+    await data.createNewData(req, res);
 });
 
 router.post('/update', (req, res) => {
-    data.updateData(req, res);
+    await data.updateData(req, res);
 });
 
 module.exports = router;
