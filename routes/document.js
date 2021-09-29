72d4a22773fd4f15aed52320b3065349826c4438
AreonL <areon.lundkvist@gmail.com>
2021-09-29 13:17:43 +0200

    updated document.js

--- routes/document.js
+++ routes/document.js
@@ -3,15 +3,15 @@ var router = express.Router();
 const data = require('../models/data');
 
 router.get('/', async function(req, res) {
-    await data.getAllData(req, res);
+    return data.getAllData(req, res);
 });
 
 router.post('/create', (req, res) => {
-    await data.createNewData(req, res);
+    return data.createNewData(req, res);
 });
 
 router.post('/update', (req, res) => {
-    await data.updateData(req, res);
+    return data.updateData(req, res);
 });
 
 module.exports = router;
